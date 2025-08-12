a6a3d5d433a325013cbfe89a4bfd6b973de4d081
marioca221 <marialuizaalmeidapou@gmail.com>
2025-08-11 21:18:38 -0300

    AULA

--- aula_0/wsgi.py
+++ aula_0/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for aula_introducao project.
+WSGI config for aula_0 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aula_introducao.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aula_0.settings')
 
 application = get_wsgi_application()
